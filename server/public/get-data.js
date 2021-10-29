9bda208cededcfae03769846bc492b8a9b7bf0b4
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2021-10-29 11:33:36 -0700

    Get data from GA by variant

--- server/public/get-data.js
+++ server/public/get-data.js
@@ -8,52 +8,81 @@
     method: "POST"
   };
 
-  window.queryReports = () => {
-    gapi.client
-      .request({
-        ...API_INFO,
-        body: {
-          reportRequests: [
-            {
-              viewId: VIEW_ID,
-              dateRanges: [
-                {
-                  startDate: "14daysAgo",
-                  endDate: "7daysAgo"
-                },
-                {
-                  startDate: "7daysAgo",
-                  endDate: "today"
-                }
-              ],
-              metrics: [
-                {
-                  expression: "ga:pageviews"
-                },
-                {
-                  expression: "ga:uniquePageviews"
-                },
-                {
-                  expression: "ga:avgTimeOnPage"
-                },
-                {
-                  expression: "ga:bounceRate"
-                }
-              ],
-              dimensions: [
-                {
-                  name: "ga:deviceCategory"
-                }
-              ]
-            }
-          ]
-        }
-      })
-      .then(displayResults, console.error.bind(console));
+  const METRICS = [
+    "pageviews",
+    "uniquePageviews",
+    "avgTimeOnPage",
+    "bounceRate",
+    "exitRate"
+  ];
+
+  const THIS_WEEK = {
+    startDate: "7daysAgo",
+    endDate: "today"
+  };
+  const LAST_WEEK = {
+    startDate: "14daysAgo",
+    endDate: "7daysAgo"
   };
 
-  function displayResults(response) {
-    var formattedJson = JSON.stringify(response.result.reports[0], null, 2);
-    document.getElementById("query-output").value = formattedJson;
-  }
+  const runQuery = metrics =>
+    gapi.client.request({
+      ...API_INFO,
+      body: {
+        reportRequests: [
+          {
+            viewId: VIEW_ID,
+            dateRanges: [LAST_WEEK, THIS_WEEK],
+            metrics: metrics.map(name => ({ expression: `ga:${name}` })),
+            dimensions: [
+              {
+                name: "ga:deviceCategory"
+              },
+              {
+                name: "ga:pagePath"
+              }
+            ],
+            dimensionFilterClauses: [
+              {
+                filters: [
+                  {
+                    dimensionName: "ga:pagePath",
+                    expressions: ["/\\w+$"]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    });
+
+  const queryGA = async metrics => {
+    try {
+      const response = await runQuery(metrics);
+      const report = response.result.reports[0];
+
+      const data = {};
+      report.data.rows.forEach(row => {
+        const deviceType = row.dimensions[0];
+        const variant = row.dimensions[1];
+        row.metrics.forEach((point, i) => {
+          point.values.forEach((value, j) => {
+            _.set(
+              data,
+              [variant, deviceType, metrics[j], i],
+              parseFloat(value)
+            );
+          });
+        });
+      });
+
+      var formattedJson = JSON.stringify(data, null, 2);
+      document.getElementById("query-output").value = formattedJson;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  window.queryReports = () => queryGA(METRICS);
 })();
