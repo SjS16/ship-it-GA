3a60dad1d00fe4bfe4b8ba447ba1d83036888833
SjS16 <stephsimpson911@gmail.com>
2021-10-29 14:24:59 -0700

    add visual tables

--- server/public/get-data.js
+++ server/public/get-data.js
@@ -5,7 +5,7 @@
   const API_INFO = {
     path: "/v4/reports:batchGet",
     root: "https://analyticsreporting.googleapis.com/",
-    method: "POST"
+    method: "POST",
   };
 
   const METRICS = [
@@ -13,23 +13,23 @@
     "uniquePageviews",
     "avgTimeOnPage",
     "bounceRate",
-    "exitRate"
+    "exitRate",
   ];
 
   const THIS_WEEK = {
     startDate: "7daysAgo",
-    endDate: "today"
+    endDate: "today",
   };
   const LAST_WEEK = {
     startDate: "14daysAgo",
-    endDate: "7daysAgo"
+    endDate: "7daysAgo",
   };
   const LAST_YEAR = {
     startDate: "365daysAgo",
-    endDate: "today"
+    endDate: "today",
   };
 
-  const runQuery = metrics =>
+  const runQuery = (metrics) =>
     gapi.client.request({
       ...API_INFO,
       body: {
@@ -37,37 +37,37 @@
           {
             viewId: VIEW_ID,
             dateRanges: [LAST_YEAR],
-            metrics: metrics.map(name => ({ expression: `ga:${name}` })),
+            metrics: metrics.map((name) => ({ expression: `ga:${name}` })),
             dimensions: [
               {
-                name: "ga:deviceCategory"
+                name: "ga:deviceCategory",
               },
               {
-                name: "ga:pagePath"
-              }
+                name: "ga:pagePath",
+              },
             ],
             dimensionFilterClauses: [
               {
                 filters: [
                   {
                     dimensionName: "ga:pagePath",
-                    expressions: ["/\\w+$"]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
+                    expressions: ["/\\w+$"],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     });
 
-  const queryGA = async metrics => {
+  const queryGA = async (metrics) => {
     try {
       const response = await runQuery(metrics);
       const report = response.result.reports[0];
 
       const data = {};
-      report.data.rows.forEach(row => {
+      report.data.rows.forEach((row) => {
         const deviceType = row.dimensions[0];
         const variant = row.dimensions[1];
         row.metrics.forEach((point, i) => {
@@ -80,13 +80,69 @@
           });
         });
       });
-
+      // createTable(data);
+      for (let variant in data) {
+        createTable(variant, data[variant]);
+      }
       var formattedJson = JSON.stringify(data, null, 2);
       document.getElementById("query-output").value = formattedJson;
     } catch (e) {
       console.error(e);
     }
   };
+
+  function createTable(title, data) {
+    const body = document.body;
+
+    //variant title
+    const h = document.createElement("H1");
+    const t = document.createTextNode(`Variant: ${title}`);
+    h.appendChild(t);
+    body.appendChild(h);
+
+    //create table
+    var tbl = document.createElement("table");
+    tbl.className = "gaData";
+    tbl.style.width = "100px";
+    tbl.style.border = "1px solid black";
+    const header = tbl.createTHead();
+    const row = header.insertRow(0);
+
+    //set up data
+    let devices = Object.keys(data);
+    let headers = Object.keys(data[devices[0]]);
+
+    //add in device header row
+    let tr = tbl.insertRow(0);
+    const th = document.createElement("th"); // TABLE HEADER.
+    th.innerHTML = "Device";
+    tr.appendChild(th);
+
+    //set headers
+    for (let i = 0; i < headers.length; i++) {
+      const th = document.createElement("th"); // TABLE HEADER.
+      th.innerHTML = toSentenceCase(headers[i]);
+      tr.appendChild(th);
+    }
+
+    for (let i = 0; i < devices.length; i++) {
+      let tr = tbl.insertRow(-1);
+      const cell = tr.insertCell(-1);
+      cell.innerHTML = toSentenceCase(devices[i]);
+      for (let j = 0; j < headers.length; j++) {
+        const cell = tr.insertCell(-1);
+        cell.innerHTML = data[devices[i]][headers[j]];
+      }
+    }
+
+    body.appendChild(tbl);
+  }
+
+  function toSentenceCase(word) {
+    var result = word.replace(/([A-Z])/g, " $1");
+    var finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+    return finalResult;
+  }
 
   window.queryReports = () => queryGA(METRICS);
 })();
